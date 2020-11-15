abb64f4d52052144efffd272d652ca9ddba004cf
baptistemrt <contact@baptistemrt.fr>
2020-11-15 17:13:03 +0100

    Check uid is from params or body

--- api/src/middlewares/auth.middleware.js
+++ api/src/middlewares/auth.middleware.js
@@ -8,15 +8,16 @@ require('dotenv').config();
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
+    console.log(token);
     const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN_JWT);
     const uid = decodedToken.uid;
-    if (req.params.uid !== uid)
+    if ((req.params.uid || req.body.uid) !== uid)
       throw "Invalid uid";
     else
       next();
   } catch {
     res.status(401).send({
-      error: new Error('Invalid Request')
+      error: "Invalid Request"
     });
   }
 };
