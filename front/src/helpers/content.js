799119e282c0fa1c25550805b38f61bfc6512177
baptistemrt <contact@baptistemrt.fr>
2020-11-22 20:52:51 +0100

    Add function to call just one film

--- front/src/helpers/content.js
+++ front/src/helpers/content.js
@@ -1,6 +1,7 @@
 import Cookies from 'js-cookie';
 
 var allFilms = {};
+var film = {};
 
 const Content = {
   async getAllFilms() {
@@ -18,6 +19,23 @@ const Content = {
         return data;
       })
     return (allFilms);
+  },
+
+  async getFilm(film_uid) {
+    film = await fetch(`${process.env.REACT_APP_API_HOST}/film/${film_uid}/${Cookies.get('uid')}`, {
+      method: 'GET',
+      headers: {
+        'Accept': '*/*',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + Cookies.get('access_token'),
+        'Access-Control-Allow-Origin': "*"
+      }
+      })
+      .then(res => res.json())
+      .then(data => {
+        return data;
+      })
+    return (film[0]);
   }
 }
 
